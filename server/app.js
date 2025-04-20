da1828ae6031dda2e08138274f31d6da6d00ec0b
adeelfeb <at4563323@gmail.com>
2025-04-20 22:40:35 +0500

    feat: - add postman_collection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import path from 'path'; // Import the 'path' module
 import { fileURLToPath } from 'url'; // Needed for __dirname in ES Modules
-import { redirectToOriginalUrl, renderErrorPage, renderHomePage } from './controllers/url.controller.js';  
 
 
 
@@ -11,11 +10,10 @@ const __dirname = path.dirname(__filename);
 // --- ---
 
 const app = express();
-const router = express.Router();
 
 // --- Middleware ---
-app.use(express.json({ limit: '16kb' }));
-app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.json({ limit: '64kb' }));
+app.use(express.urlencoded({ extended: true, limit: '64kb' }));
 
 // --- View Engine Setup ---
 app.set('view engine', 'ejs');  
